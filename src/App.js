ae2bb418161148a5965e63bdb59fdd3ce0a23ba3
unknown <7036@TRADEVANAD.INTRANET.LOCAL>
2023-12-13 16:19:40 +0800

    use React Router for page transitions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from 'pages/HomePage';
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/material" element={<HomePage />} />
+        <Route path="/bootstrap" element={<HomePage />} />
       </Routes>
     </Router>
   );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,26 +1,40 @@
 import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import MaterialForm from 'components/MaterialForm.js';
 import BootstrapForm from 'components/BootstrapForm.js';
 
 
 const HomePage = () => 
 {
-const [mode, setMode] = useState(true);
+    // const [mode, setMode] = useState(false);
 
-const toggleMode = () => {
-setMode(!mode); // 切換 mode 的值
-};
+    // const toggleMode = () => {
+    //     setMode(!mode);
+    // };
 
-return (
-<div>
-<h1 onClick={toggleMode} style={{ textAlign: 'center', cursor: 'pointer' }}>
-{mode ? 'BootstrapForm' : 'MaterialForm'}
-</h1>
-<div style={{width: '900px', margin: 'auto'}}>
-{mode ? <BootstrapForm /> : <MaterialForm />}
-</div>
-</div>
-);
+    const location = useLocation();
+    
+    const theme = 
+        location.pathname.includes('/material') ? 'material' :
+        location.pathname.includes('/bootstrap') ? 'bootstrap' 
+        : 'default';
+
+    return (
+        <div>
+            <h1 style={{ textAlign: 'center' }}>
+                {theme === 'material' ? 'MaterialForm' : theme === 'bootstrap' ? 'BootstrapForm' : 'WelcomeHome'}
+            </h1>
+            <div style={{ textAlign: 'center', marginBottom: '10px' }}>
+                <a href='/material'>Material</a>&nbsp;&nbsp;&nbsp;
+                <a href='/'>回首頁</a>&nbsp;&nbsp;&nbsp;
+                <a href='/bootstrap'>Bootstrap</a>
+            </div>
+            <div style={{ width: '900px', margin: 'auto' }}>
+                {theme === 'material' && <MaterialForm />}
+                {theme === 'bootstrap' && <BootstrapForm />}
+            </div>
+        </div>
+    );
 }
 
 export default HomePage;
